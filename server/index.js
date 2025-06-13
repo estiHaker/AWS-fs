aca7fcbd07e9e0fe14ad810c1a3cb7203e496735
estiHaker <144729995+estiHaker@users.noreply.github.com>
2025-06-13 16:17:43 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,6 +9,6 @@ app.get('/api/hello', (req, res) => {
     res.json({ message: 'Hello from backend!' });
 });
 
-apapp.listen(port, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server listening on port ${port}`);
 });
